61c4cd72ec7e6da8ad795b9e4171639d3c08ca8e
Naga Deepak Kaza <83226719+nagadeepak511@users.noreply.github.com>
2023-08-02 14:35:50 +0530

    Create variable_size_sliding_window.cpp

---
+++ variable_size_sliding_window.cpp
@@ -0,0 +1,16 @@
+// Problem statement:
+/*
+    Find longest/smallest subarray/substring satisfying given condition
+*/
+
+int i=0, j=0;
+while(j<n){
+    // add j to the sliding window
+    // ...
+    while(/* condition is satisfied */){
+        // do calculation and update answer
+        // ...
+        i++; // slide the window
+    }
+    j++;
+}
