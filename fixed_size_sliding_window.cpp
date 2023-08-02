b22fa97bfb04464dacd14eb4aa89307f5db6978c
Naga Deepak Kaza <83226719+nagadeepak511@users.noreply.github.com>
2023-08-02 14:31:22 +0530

    Create fixed_size_sliding_window.cpp
    Template for fixed size sliding window in c++

---
+++ fixed_size_sliding_window.cpp
@@ -0,0 +1,11 @@
+int i=0,j=0; // start and end of the sliding window
+while(j<n){
+    // add j to window
+    // ...
+    if(j-i+1 < k) j++; // maintain the size
+    else if(j-i+1 == k){
+        // calculation and update answer
+        // ...
+        i++, j++; // slide the window
+    }
+}
